5903ee22c1af3b2950a174f3c7a6c232a7c4a77f
eric <dengerpeng@tanlunet.com>
2019-05-29 20:23:29 +0800

    底部菜单栏

---
+++ baseui/src/main/java/com/eric/baseui/RadioMenuBar.java
@@ -0,0 +1,20 @@
+package com.eric.baseui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class RadioMenuBar extends View {
+
+    public RadioMenuBar(Context context) {
+        super(context);
+    }
+
+    public RadioMenuBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RadioMenuBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
