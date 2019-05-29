e2700c1dc0c0fab6bbc11718d748d848fe2330c1
eric <dengerpeng@tanlunet.com>
2019-05-29 18:15:05 +0800

    基础 UI库

---
+++ baseui/src/main/java/com/eric/baseui/TitleBar.java
@@ -0,0 +1,25 @@
+package com.eric.baseui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class TitleBar extends RelativeLayout {
+    public TitleBar(Context context) {
+        super(context);
+
+    }
+
+    public TitleBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+//        inflate(context,R.layout.view_title,this);
+    }
+
+    public TitleBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+//    private void init(Context context,AttributeSet attrs){
+//        inflate(context,R.layout.,this);
+//
+//    }
+}
